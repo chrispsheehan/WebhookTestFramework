02bbda04cc3e95e2fb87a76e61aa574d2626646e
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2021-02-15 10:48:05 +0000

    recird webhook

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
     echo "Hello, you badger";
 
-    file_put_contents('/tmp/ngrok/ngroktest.log', json_encode($_POST));
+    file_put_contents('/tmp/ngroktest.log', json_encode($_POST));
 ?> 
